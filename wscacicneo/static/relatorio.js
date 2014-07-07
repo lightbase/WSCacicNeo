717a350b886ff3ab2adce81915128c390714aec2
Helder <heldim.ufv@gmail.com>
2014-07-07 10:51:33 -0300

    Correção da tela relatórios

--- wscacicneo/static/relatorio.js
+++ wscacicneo/static/relatorio.js
@@ -77,21 +77,16 @@ Ext.onReady(function(){
 
 });
 
-
-// **** GRAFICO DE COLUNA ****
+// **** GRAFICO DE PIZZA ****
 
 var store = Ext.create('Ext.data.JsonStore', {
     fields: ['name', 'data'],
     data: [
-        { 'name': 'dado1', 'data':  200 },
-        { 'name': 'dado2', 'data':  20 },
-        { 'name': 'dado3', 'data':  500 },
-        { 'name': 'dado4', 'data':  600 },
-        { 'name': 'dado5', 'data':  700 },
-        { 'name': 'dado6', 'data':  1000 },
-        { 'name': 'dado7', 'data':  50 },
-        { 'name': 'dado8', 'data':  200 },
-        { 'name': 'dado9', 'data':  400 },
+        { 'name': 'dado1', 'data':  2 },
+        { 'name': 'dado2', 'data':  2 },
+        { 'name': 'dado3', 'data':  4 },
+        { 'name': 'dado4', 'data': 10 },
+        { 'name': 'dado5', 'data': 20 }
 ]});
 
 var chart = Ext.create('Ext.chart.Chart', {
@@ -102,55 +97,43 @@ var chart = Ext.create('Ext.chart.Chart', {
     store: store,
     theme: 'Base:gradients',
     shadow: true,
-    axes: [
-        {
-            type: 'Numeric',
-            position: 'left',
-            fields: ['data'],
-            label: {
-                renderer: Ext.util.Format.numberRenderer('0,0')
-            },
-            title: 'Eixo X',
-            grid: true,
-            minimum: 0
+    legend: {
+        position: 'right'
+    },
+    series: [{
+        type: 'pie',
+        angleField: 'data',
+        showInLegend: true,
+        tips: {
+            trackMouse: true,
+            width: 140,
+            height: 28,
+            renderer: function(storeItem, item) {
+                // calculate and display percentage on hover
+                var total = 0;
+                store.each(function(rec) {
+                    total += rec.get('data');
+                });
+                this.setTitle(storeItem.get('name') + ': ' + Math.round(storeItem.get('data') / total * 100) + '%');
+            }
         },
-        {
-            type: 'Category',
-            position: 'bottom',
-            fields: ['name'],
-            title: 'Eixo Y'
+        highlight: {
+            segment: {
+                margin: 20
+            }
+        },
+        label: {
+            field: 'name',
+            display: 'rotate',
+            contrast: true,
+            font: '18px Arial'
         }
-    ],
-    series: [
-        {
-            type: 'column',
-            axis: 'left',
-            highlight: true,
-            tips: {
-              trackMouse: true,
-              width: 140,
-              height: 28,
-              renderer: function(storeItem, item) {
-                this.setTitle(storeItem.get('name') + ': ' + storeItem.get('data'));
-              }
-            },
-            label: {
-              display: 'insideEnd',
-              'text-anchor': 'middle',
-                field: 'data',
-                renderer: Ext.util.Format.numberRenderer('0'),
-                orientation: 'vertical',
-                color: '#333'
-            },
-            xField: 'name',
-            yField: 'data'
-        }
-    ]
+    }]
 });
 
 widget = Ext.create('Ext.panel.Panel', {
         layout: 'fit',
-        title: 'Grafico de dados',
+        title: 'Relatório',
         width: '75%',
         frame: true,
         draggable: true,
@@ -173,3 +156,5 @@ Ext.onReady(function(){
         });
 
 });
+
+
