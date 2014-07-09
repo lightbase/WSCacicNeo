a15b7fd159ee9d364c4d8627e19dfb7f64225210
Eduardo <eduardo.gcr12@gmail.com>
2014-07-09 12:49:46 -0300

    correçoes

--- wscacicneo/static/main.js
+++ wscacicneo/static/main.js
@@ -13,14 +13,13 @@ Ext.onReady(function() {
                         {text: 'Gestor de Orgãos' },
                         {text: 'Notificações' },
                         {text: 'Relatórios' },
-                        {text: 'Acesso Rápido' },
                         {text: 'Sobre' },
                        ]
         });
 
         fav_html = '<div id="favoriteItems">'+
                 '<div class="fav"><a href="reports"><img src="static/icons/relatorios.png">Relat&oacuterios</a></div>'+
-                '<div class="fav"><a><img src="static/icons/usuario.png">Usu&aacuterio</a></div>'+
+                '<div class="fav"><a><img src="static/icons/usuario.png">Meu Perfil</a></div>'+
                 '<div class="fav"><a><img src="static/icons/ferramentas.png">Ferramentas de sistema</a></div>'+
         '</div>';
 
