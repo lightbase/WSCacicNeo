3fbe9cd58cdabaf6a5e6ee0f056f125e482888f1
Adley <adleydamaceno@gmail.com>
2015-08-28 00:58:22 -0300

    Correçao do teste unitário do blacklist

--- wscacicneo/test/blacklist/test_unit_blacklist.py
+++ wscacicneo/test/blacklist/test_unit_blacklist.py
@@ -17,38 +17,39 @@ class TestAttributesBlacklist(unittest.TestCase):
 
     def setUp(self):
         """
-        :return: Inicializa o Servidor de Teste
+         Inicializa o Servidor de Teste
         """
         from wscacicneo import main
         app = main({}, **settings)
         from webtest import TestApp
         self.testapp = TestApp(app)
-        # from wscacicneo.model import blacklist
-        #
-        # blacklist_base = blacklist.BlacklistBase()
-        # result = blacklist_base.create_base()
-        # self.assertEqual(result, True)
+        from wscacicneo.model import blacklist
+
+        """
+        Cria a base da lista de eliminação (blacklist)
+        """
+        blacklist_base = blacklist.BlacklistBase()
+        result = blacklist_base.create_base()
+        self.assertEqual(result, True)
 
     def test_insert_admin_blacklist(self):
         """
-        :return: Insere um Item na base Blacklist
+         Insere um Item na base Blacklist
         """
         from wscacicneo.model import blacklist
-        from wscacicneo.utils.utils import Utils
         from wscacicneo.test.security.test_profile import TestProfile
         ''' Verifica se possui permissão de Admin '''
         TestProfile.test_permission_administrator(self)
         ''' Gera uma string aleatória '''
-        random_name = Utils.random_string(8)
         blacklist_obj = blacklist.Blacklist(
-            item=random_name
+            item="Microsoft Office 2007"
         )
         results = blacklist_obj.create_item()
         self.assertEqual(type(results), int)
 
     def test_delete_admin_blacklist(self):
         """
-        :return: Remove um Item da base Blacklist
+         Remove um Item da base Blacklist
         """
         from wscacicneo.model import blacklist
         from wscacicneo.utils.utils import Utils
@@ -66,11 +67,11 @@ class TestAttributesBlacklist(unittest.TestCase):
 
     def test_list_items_blacklist(self):
         """
-        :return: Lista todos os itens da base Blacklist
+         Lista todos os itens da base Blacklist
         """
         from wscacicneo.model import blacklist
         from wscacicneo.test.security.test_profile import TestProfile
-        import collections
+
         ''' Verifica se possui permissão de Admin '''
         TestProfile.test_permission_administrator(self)
         blacklist_obj = blacklist.Blacklist(
@@ -82,10 +83,13 @@ class TestAttributesBlacklist(unittest.TestCase):
 
     def tearDown(self):
         """
-        :return: Apaga dados do Teste
+         Apaga dados do Teste
         """
-        # from wscacicneo.model import blacklist
-        #
-        # blacklist_base = blacklist.BlacklistBase()
-        # result = blacklist_base.remove_base()
-        # self.assertEqual(result, True)
+        from wscacicneo.model import blacklist
+
+        """
+        Exclui a base da lista de eliminação (blacklist)
+        """
+        blacklist_base = blacklist.BlacklistBase()
+        result = blacklist_base.remove_base()
+        self.assertEqual(result, True)
